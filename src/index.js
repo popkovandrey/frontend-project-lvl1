401be0176a5f8bdb6ee9dbad56ff0cccc71971a7
Andrey <andrewku@yandex.ru>
2019-11-30 00:41:37 +0500

    add export default in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 // функция возвращающая случайное целое число в дапазоне от мин до макс
-export const rndInRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const rndInRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+export default rndInRange;
